2516f77292527a58dc4dc28eb4804922f786ddfc
BACOUL <102251510+BACOUL@users.noreply.github.com>
2025-08-21 16:40:51 +0200

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,1 +1,26 @@
+// pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document';
 
+export default function Document() {
+  return (
+    <Html lang="fr">
+      <Head>
+        {/* Fonts (Inter + Sora) */}
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500&family=Sora:wght@600;700&display=swap"
+          rel="stylesheet"
+        />
+
+        {/* Favicons / PWA */}
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+    }
