d90ca9dbb4c0f717ad95002cb2e6bea3e0a1585b
BACOUL <102251510+BACOUL@users.noreply.github.com>
2025-08-21 12:30:47 +0200

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,20 +1,10 @@
-import { defineConfig, devices } from '@playwright/test';
-
-const PORT = 3000;
+import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-  testDir: './tests',
-  timeout: 30_000,
-  retries: 0,
-  use: {
-    baseURL: process.env.BASE_URL || `http://localhost:${PORT}`,
-    trace: 'on-first-retry',
-    ...devices['Desktop Chrome'],
-  },
-  // Lance ton site pour les tests (build + start)
   webServer: {
-    command: 'npm run build && npm run start',
-    port: PORT,
-    reuseExistingServer: !process.env.CI
-  }
+    command: 'npm run dev',
+    port: 3000,
+    reuseExistingServer: !process.env.CI,
+  },
+  testDir: 'tests',
 });
