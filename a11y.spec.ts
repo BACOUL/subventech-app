128834e1fe7d8b6c4559f635fb0b40cbafc9338d
BACOUL <102251510+BACOUL@users.noreply.github.com>
2025-08-21 13:00:17 +0200

    Update a11y.spec.ts

--- a11y.spec.ts
+++ a11y.spec.ts
@@ -1,7 +1,7 @@
 // tests/a11y.spec.ts
 import { test, expect } from '@playwright/test';
 
-test('Accueil Subventech se charge et a un titre', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
-  await expect(page).toHaveTitle(/Subventech/i);
+test('Accueil SubvenTech a un titre', async ({ page }) => {
+  await page.goto('/');
+  await expect(page).toHaveTitle(/Subven/i);
 });
